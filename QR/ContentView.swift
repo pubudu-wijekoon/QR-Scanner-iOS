5d90d99f56c0fcd56485181260a1cf380941e61b
Pubudu Wijekoon <37551112+pubudu-wijekoon@users.noreply.github.com>
2024-02-06 11:55:04 +0900

    Initial Commit

---
+++ QR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QR
+//
+//  Created by Pubudu Wijekoon on 2024/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QR/QRApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRApp.swift
+//  QR
+//
+//  Created by Pubudu Wijekoon on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct QRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
